3a7265799a6da07a40d365c2df2fb0e6ffc56a40
David Krause <krause@biochem2.uni-frankfurt.de>
2020-10-29 07:37:24 +0100

    Signed-off-by: David Krause <krause@biochem2.uni-frankfurt.de>

--- nimSocks/server.nim
+++ nimSocks/server.nim
@@ -364,7 +364,6 @@ proc processClient(proxy: SocksServer, client: AsyncSocket): Future[void] {.asyn
 
 proc serve*(proxy: SocksServer): Future[void] {.async.} =
   proxy.serverSocket.setSockOpt(OptReusePort, true)
-  # proxy.serverSocket.setSockOpt(OptReuseAddr, true)
   proxy.serverSocket.bindAddr(proxy.listenPort, proxy.listenHost)
   proxy.serverSocket.listen()
   var
