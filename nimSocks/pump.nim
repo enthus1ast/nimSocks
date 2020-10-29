e37c6ad3aad664e87d00b96c42cfef2931f9ae3a
David Krause <krause@biochem2.uni-frankfurt.de>
2020-10-29 09:42:43 +0100

    avoid sending 1 byte network packages => less system calls, less packages
    Signed-off-by: David Krause <krause@biochem2.uni-frankfurt.de>

--- nimSocks/pump.nim
+++ nimSocks/pump.nim
@@ -3,8 +3,14 @@ from ressourceInfo import Ressource
 
 proc pump*(proxy: SocksServer, s1, s2: AsyncSocket, direction: Direction, ressource: seq[byte], atyp: ATYP): Future[void] {.async.} =
   var buffer = newStringOfCap(SIZE)
+  # var haveExbyte = false
+  var exbyte: string # for avoiding sending 1 byte packages
   while not (s1.isClosed() and s2.isClosed()):
     buffer.setLen 0
+    if exbyte.len > 0:
+      # when we have an extra byte (from waiting),
+      # we add it here to avoid sending 1 byte network packages
+      buffer.add exbyte
     try:
       ## Peek, so input buffer remains the same!
       buffer.add await s1.recv(SIZE, flags={SocketFlag.Peek, SocketFlag.SafeDisconn})
@@ -14,13 +20,16 @@ proc pump*(proxy: SocksServer, s1, s2: AsyncSocket, direction: Direction, ressou
 
     if buffer.len > 0:
       try:
-        discard await s1.recv(buffer.len) # TODO (better way?) we empty the buffer by reading it
+        discard await s1.recv(buffer.len - exbyte.len) # TODO (better way?) we empty the buffer by reading it
+        exbyte.setLen(0)
       except:
         dbg 2, getCurrentExceptionMsg()
         buffer.setLen 0
     else:
       try:
-        buffer = await s1.recv(1) # we wait for new data...
+        exbyte = (await s1.recv(1)) # we wait for new data...
+        if exbyte.len == 1:
+          continue # the 1 byte get appended to the buffer in the next iteration
       except:
         dbg 3, getCurrentExceptionMsg()
         buffer.setLen 0
@@ -40,6 +49,7 @@ proc pump*(proxy: SocksServer, s1, s2: AsyncSocket, direction: Direction, ressou
         proxy.transferedBytes = 0 # reset if overflow
 
       try:
+        # echo buffer.len
         await s2.send(buffer)
       except:
         dbg 5, getCurrentExceptionMsg()
